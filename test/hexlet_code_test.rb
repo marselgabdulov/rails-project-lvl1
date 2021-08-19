4fb2a59a38d72e7cd2b3e816af839aafcd08c301
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2021-08-19 10:47:04 +0300

    refactor test file with rubocop

--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -8,26 +8,26 @@ class HexletCodeTest < Minitest::Test
   end
 
   def setup
-    @tag = HexletCode::Tag.new()
+    @tag = HexletCode::Tag.new
   end
 
   def test_simple_tag
-    @tag.build('br')
-    assert_equal '<br>', @tag.tag_string
+    @tag.build("br")
+    assert_equal "<br>", @tag.tag_string
   end
 
   def test_complex_tag
-    @tag.build('input', type: 'submit', value: 'Save')
+    @tag.build("input", type: "submit", value: "Save")
     assert_equal '<input type="submit" value="Save">', @tag.tag_string
   end
 
   def test_simple_paired_tag
-    @tag.build('p') { 'Hello' }
-    assert_equal '<p>Hello</p>', @tag.tag_string
+    @tag.build("p") { "Hello" }
+    assert_equal "<p>Hello</p>", @tag.tag_string
   end
 
   def test_complex_paired_tag
-    @tag.build('label', for: 'email') { 'Email' }
+    @tag.build("label", for: "email") { "Email" }
     assert_equal '<label for="email">Email</label>', @tag.tag_string
   end
 end
