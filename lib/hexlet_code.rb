d7d780b23f362c587d9aabe69e2d39aeea431649
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2021-08-19 10:52:12 +0300

    refactor lib file with rubocop

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,30 +5,27 @@ require_relative "hexlet_code/version"
 module HexletCode
   class Error < StandardError; end
   # Your code goes here...
-
   class Tag
     attr_accessor :tag_string
 
-    def initialize(tag = '')
+    def initialize(tag = "")
       @tag = tag
     end
 
     def parse_attrs(attrs)
-      result = ''
+      result = ""
       attrs.each do |a|
         a.each { |k, v| result += "#{k}=\"#{v}\" " }
       end
       result.rstrip
     end
 
-    def build(tag_name, *args)
-      @tag_string = "<#{tag_name}>#{yield}</#{tag_name}>" if block_given? && args.empty?
-      @tag_string = "<#{tag_name} #{parse_attrs(args)}>#{yield}</#{tag_name}>" if block_given? && !args.empty?
-      @tag_string = "<#{tag_name} #{parse_attrs(args)}>" if !block_given? && !args.empty?
-      @tag_string = "<#{tag_name}>" if !block_given? && args.empty?
+    def build(name, *args)
+      @tag_string = "<#{name}>#{yield}</#{name}>" if block_given? && args.empty?
+      @tag_string = "<#{name} #{parse_attrs(args)}>#{yield}</#{name}>" if block_given? && !args.empty?
+      @tag_string = "<#{name} #{parse_attrs(args)}>" if !block_given? && !args.empty?
+      @tag_string = "<#{name}>" if !block_given? && args.empty?
     end
   end
 end
-
-tag = HexletCode::Tag.new()
 
