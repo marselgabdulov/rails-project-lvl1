2c253d9c192e8e39017f29195f7046b045ba5c81
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2021-08-31 15:58:09 +0300

    remove old tests

--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -14,23 +14,7 @@ class HexletCodeTest < Minitest::Test
     @user = User.new name: "rob", job: "hexlet", gender: "m"
   end
 
-  def test_simple_tag
-    assert_equal "<br>", @tag.to_html("br")
-  end
-
-  def test_complex_tag
-    assert_equal '<input type="submit" value="Save">', @tag.to_html("input", type: "submit", value: "Save")
-  end
-
-  def test_simple_paired_tag
-    assert_equal "<p>Hello</p>", @tag.to_html("p") { "Hello" }
-  end
-
-  def test_complex_paired_tag
-    assert_equal '<label for="email">Email</label>', @tag.to_html("label", for: "email") { "Email" }
-  end
-
-  def test_form_for_finale
+  def test_form_for
     form = HexletCode.form_for @user do |f|
       f.input :name
       f.input :job, as: :text
