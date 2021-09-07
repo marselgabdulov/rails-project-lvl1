021d5bd8787de994d148693c5278c8fa472b9230
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2021-09-08 01:04:51 +0300

    fix: rubocop

--- test/fixtures/form_fixture.rb
+++ test/fixtures/form_fixture.rb
@@ -2,7 +2,9 @@
 
 # module FormFixture
 module FormFixture
+  # rubocop:disable Layout/LineLength
   def self.full_form
     '<form action="#" method="post"><label for="name">Name</label><input type="text" value="rob" name="name"><label for="job">Job</label><textarea cols="50" rows="50" name="job">hexlet</textarea><select name="gender"><option value="m" selected>m</option><option value="f">f</option></select><input type="submit" value="Save" name="commit"></form>'
   end
+  # rubocop:enable Layout/LineLength
 end
