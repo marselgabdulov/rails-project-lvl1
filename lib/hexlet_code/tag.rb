47a62dcd999a28ec84870ceec9d3a4f76c242d07
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2021-09-23 19:04:50 +0300

    refactor: privataze Tag.parse_attrs

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -4,12 +4,6 @@
 module HexletCode
   # module Tag
   module Tag
-    def self.parse_attrs(attrs)
-      attrs.inject(:merge).map do |key, value|
-        "#{key}=\"#{value}\""
-      end.join(' ')
-    end
-
     def self.to_html(tag, *args)
       single_tags = %w[br img input]
       html = "<#{tag} #{parse_attrs(args)}>"
@@ -21,5 +15,14 @@ module HexletCode
         "<#{tag} #{parse_attrs(args)}></#{tag}>"
       end
     end
+
+    private
+
+    def self.parse_attrs(attrs)
+      attrs.inject(:merge).map do |key, value|
+        "#{key}=\"#{value}\""
+      end.join(' ')
+    end
+
   end
 end
