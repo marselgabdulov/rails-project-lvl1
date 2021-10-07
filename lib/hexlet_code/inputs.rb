6abf9189871a533977e3579b1931a6a060a7acd5
Artem Krivonozhko <artemkrivonozhko@gmail.com>
2021-10-07 10:18:19 +0300

    Use autoload, fix namespaces for inputs

---
+++ lib/hexlet_code/inputs.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# module HexletCode
+module HexletCode
+  # module Inputs
+  module Inputs
+    autoload :BaseInput, 'hexlet_code/inputs/base_input.rb'
+    autoload :Input, 'hexlet_code/inputs/input.rb'
+    autoload :Select, 'hexlet_code/inputs/select.rb'
+    autoload :TextArea, 'hexlet_code/inputs/text_area.rb'
+  end
+end
